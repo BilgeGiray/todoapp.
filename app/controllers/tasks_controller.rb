3e0e77c79be8e74ee4bd02ca092a7921cb133a75
BilgeGiray <gry33b@gmail.com>
2013-12-03 14:23:14 +0100

    Todo und Done geordnet

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,9 @@ class TasksController < ApplicationController
   # GET /tasks.json
   def index
     @done = Task.where(done: true)
+    @done = Task.where(done: true).order(created_at: :desc)
     @todo = Task.where(done: false)
+    @todo = Task.where(done: false).order(updated_at: :desc)
   end
 
 
