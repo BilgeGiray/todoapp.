029b0ea5288b009ae6233e04811049090afb4293
BilgeGiray <gry33b@gmail.com>
2014-01-07 15:45:26 +0100

    Username zu User hinzugefügt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
 
+validates :username, presence:  true, uniqueness: true
 	has_many :tasks
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         	validates :username, presence:  true, uniqueness: true
 end
